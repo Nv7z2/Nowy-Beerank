d37be27061fa09d84a3fd33bb1e9dfb18798c3e0
Kacper Baran <barankacper2000@gmail.com>
2025-09-08 20:08:27 +0200

    feat: add vue page for services aggragator /oferta

---
+++ app/pages/oferta/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const route = useRoute()
+const { data: page } = await useAsyncData(route.path, () =>
+  queryCollection("pages").path('/pages/oferta').first() // Adjust the path to match your content structure
+);
+
+console.log(page.value)
+
+const metaTitle = computed(() => page?.value?.seo.title);
+const metaDesc = computed(() => page?.value?.seo.description);
+
+useSeoMeta({
+  title: metaTitle,
+  ogTitle: metaTitle,
+  twitterTitle: metaTitle,
+  description: metaDesc,
+  ogDescription: metaDesc,
+  twitterDescription: metaDesc,
+  twitterCard: "summary_large_image",
+});
+</script>
+
+<template>
+  <main>
+    <ContentRenderer v-if="page" :value="page" />
+  </main>
+</template>
