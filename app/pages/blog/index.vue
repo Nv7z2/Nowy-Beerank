998603a1a01b766a54bb08fec020f40de8871450
Kacper Baran <barankacper2000@gmail.com>
2025-09-09 00:40:09 +0200

    feat: MVP add simple blog posts aggregator

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,7 +1,24 @@
-<script setup></script>
+<script setup>
+const route = useRoute();
+const { data: page } = await useAsyncData(route.path, () => {
+  return queryCollection("blog").all();
+});
+</script>
 
 <template>
-blog
+  <div class="blog">
+    <div class="blog-box" v-for="post in page" :key="post.id">
+      <NuxtLink :to="post.path">
+        {{ post.title }}
+      </NuxtLink>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.blog {
+  &-box {
+    margin: 2rem;
+  }
+}
+</style>
