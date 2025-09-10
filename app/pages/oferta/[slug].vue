6f2a6b3a028552795f621a5c068df1f86993645a
Kacper <barankacper2000@gmail.com>
2025-09-10 22:44:29 +0200

    fix: bring back v-if for page content in service page template

--- app/pages/oferta/[slug].vue
+++ app/pages/oferta/[slug].vue
@@ -1,29 +1,32 @@
 <script setup lang="ts">
-const route = useRoute()
-const { data: page } = await useAsyncData(route.path, () =>
-  queryCollection("pages").path('/pages/oferta/' + route.params.slug).first(), {
-    initialCache: false
-  }
+const route = useRoute();
+const { data: page } = await useAsyncData(
+    route.path,
+    () =>
+        queryCollection("pages")
+            .path("/pages/oferta/" + route.params.slug)
+            .first(),
+    {
+        initialCache: false,
+    }
 );
 
-const pageSafe = computed(() => page?.value ?? {})
-
-const metaTitle = computed(() => pageSafe?.value?.seo.title);
-const metaDesc = computed(() => pageSafe?.value?.seo.description);
+const metaTitle = computed(() => page?.value?.seo.title);
+const metaDesc = computed(() => page?.value?.seo.description);
 
 useSeoMeta({
-  title: metaTitle,
-  ogTitle: metaTitle,
-  twitterTitle: metaTitle,
-  description: metaDesc,
-  ogDescription: metaDesc,
-  twitterDescription: metaDesc,
-  twitterCard: "summary_large_image",
+    title: metaTitle,
+    ogTitle: metaTitle,
+    twitterTitle: metaTitle,
+    description: metaDesc,
+    ogDescription: metaDesc,
+    twitterDescription: metaDesc,
+    twitterCard: "summary_large_image",
 });
 </script>
 
 <template>
-  <main>
-    <ContentRenderer :value="pageSafe" />
-  </main>
+    <main>
+        <ContentRenderer v-if="page" :value="pagepageSafe" />
+    </main>
 </template>
