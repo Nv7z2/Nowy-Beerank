367d42229124cda31a08cd33a45a60bb00544403
Kacper <barankacper2000@gmail.com>
2025-09-10 18:56:19 +0200

    style: change intents from 2 to 4 in nuxt config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,88 +3,88 @@ import { defineOrganization } from "nuxt-schema-org/schema";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
-  nitro: {
-    preset: "netlify",
-  },
-  target: "static",
-  telemetry: false,
-  compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/sitemap",
-    "@nuxt/content",
-    "@nuxt/fonts",
-    "@nuxt/scripts",
-    "@nuxt/image",
-    "nuxt-schema-org",
-  ],
-  alias: {
-    // Alias do folderu komponentów w app/
-    "@components": resolve(__dirname, "app/components"),
-    "@styles": resolve(__dirname, "assets/styles"),
-    "@images": resolve(__dirname, "assets/images"),
-    "@icons": resolve(__dirname, "assets/icons"),
-  },
-  image: {
-    domains: ["nowy-beerank.netlify.app", "cdn.beerank.pl"],
-    provider: "ipx",
-  },
-  features: {
-    inlineStyles: false,
-  },
-  css: ["@styles/main.scss"],
-  app: {
-    hydrate: true,
-    head: {
-      htmlAttrs: {
-        lang: "pl",
-      },
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
+    ssr: true,
+    nitro: {
+        preset: "netlify",
     },
-    pageTransition: { name: "page", mode: "out-in" },
-  },
-  fonts: {
-    families: [{ name: "Inter", provider: "google" }],
-  },
-  site: {
-    trailingSlash: false,
-    url: "https://beerank.pl",
-    name: "Beerank - Pozycjonowanie SEO i Marketing w internecie",
-  },
-  schemaOrg: {
-    identity: defineOrganization({
-      // Basic Information
-      name: "Beerank",
-      logo: "/logo.png",
-      url: "https://beerank.pl",
-      description: "Jesteśmy psz—czołową agencje SEO w Polsce",
-      email: "kontakt@beerank.pl",
-      taxID: "5842739346",
-      vatID: "PL5842739346",
-      legalName: "BeeRank - Artur Kurczewski",
-      knowsLanguage: "PL",
-      skills: "SEO, Marketing, Google Ads, Facebook Ads, Instagram Ads",
-      sameAs: [
-        "https://www.facebook.com/beerank.seo",
-        "https://instagram.com/beerank.seo",
-        "https://www.linkedin.com/company/beerank/",
-      ],
-      address: {
-        "@type": "PostalAddress",
-        addressLocality: "Gdańsk",
-        addressCountry: "PL",
-        postalCode: "80-363",
-        streetAddress: "ul. Piastowska 72B/9",
-      },
-    }),
-  },
-  vite: {
-    build: {
-      cssCodeSplit: false,
+    target: "static",
+    telemetry: false,
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    modules: [
+        "@nuxtjs/sitemap",
+        "@nuxt/content",
+        "@nuxt/fonts",
+        "@nuxt/scripts",
+        "@nuxt/image",
+        "nuxt-schema-org",
+    ],
+    alias: {
+        // Alias do folderu komponentów w app/
+        "@components": resolve(__dirname, "app/components"),
+        "@styles": resolve(__dirname, "assets/styles"),
+        "@images": resolve(__dirname, "assets/images"),
+        "@icons": resolve(__dirname, "assets/icons"),
     },
-  },
-  content: {
-    experimental: { nativeSqlite: true },
-  },
+    image: {
+        domains: ["nowy-beerank.netlify.app", "cdn.beerank.pl"],
+        provider: "ipx",
+    },
+    features: {
+        inlineStyles: false,
+    },
+    css: ["@styles/main.scss"],
+    app: {
+        hydrate: true,
+        head: {
+            htmlAttrs: {
+                lang: "pl",
+            },
+            link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
+        },
+        pageTransition: { name: "page", mode: "out-in" },
+    },
+    fonts: {
+        families: [{ name: "Inter", provider: "google" }],
+    },
+    site: {
+        trailingSlash: false,
+        url: "https://beerank.pl",
+        name: "Beerank - Pozycjonowanie SEO i Marketing w internecie",
+    },
+    schemaOrg: {
+        identity: defineOrganization({
+            // Basic Information
+            name: "Beerank",
+            logo: "/logo.png",
+            url: "https://beerank.pl",
+            description: "Jesteśmy psz—czołową agencje SEO w Polsce",
+            email: "kontakt@beerank.pl",
+            taxID: "5842739346",
+            vatID: "PL5842739346",
+            legalName: "BeeRank - Artur Kurczewski",
+            knowsLanguage: "PL",
+            skills: "SEO, Marketing, Google Ads, Facebook Ads, Instagram Ads",
+            sameAs: [
+                "https://www.facebook.com/beerank.seo",
+                "https://instagram.com/beerank.seo",
+                "https://www.linkedin.com/company/beerank/",
+            ],
+            address: {
+                "@type": "PostalAddress",
+                addressLocality: "Gdańsk",
+                addressCountry: "PL",
+                postalCode: "80-363",
+                streetAddress: "ul. Piastowska 72B/9",
+            },
+        }),
+    },
+    vite: {
+        build: {
+            cssCodeSplit: false,
+        },
+    },
+    content: {
+        experimental: { nativeSqlite: true },
+    },
 });
