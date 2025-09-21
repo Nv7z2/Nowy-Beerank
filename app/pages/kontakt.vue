699250365e108f7739aa8bc8c2271d273cc1ca06
Kacper <kacper@beerank.pl>
2025-09-21 19:47:17 +0200

    feat: add MVP /kontakt page

--- app/pages/kontakt.vue
+++ app/pages/kontakt.vue
@@ -1,7 +1,22 @@
-<script setup></script>
+<script setup>
+import ContactForm from "~/components/global/ContactForm.vue";
+</script>
 
 <template>
-kontakt
+    <div class="contact-wrapper">
+        <ContactForm />
+    </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use "@styles/variables" as *;
+@use "@styles/mixins" as *;
+
+.contact-wrapper {
+    padding: 0 $desktop-section-padding;
+
+    @include on-mobile {
+        padding: 0 $mobile-section-padding;
+    }
+}
+</style>
